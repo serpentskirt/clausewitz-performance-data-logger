626f2476f9f646dfff1041e0d6f300a8ad14996e
serpentskirt <73503+serpentskirt@users.noreply.github.com>
2019-02-22 12:20:37 +0100

    Add project files.

---
+++ clausewitz-performance-data-logger/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace clausewitz_performance_data_logger
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
